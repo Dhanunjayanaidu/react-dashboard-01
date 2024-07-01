50ecc9291166d964f86b9e6bde0bf3af5b652773
Prasad Chandra <prasadchandra6817@gmail.com>
2024-07-02 00:39:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>New React dashboard for practice 01</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Hello React Js</h2>
+        <p>Here is the practice react js dashboard from Dhanunjay</p>
       </header>
     </div>
   );
